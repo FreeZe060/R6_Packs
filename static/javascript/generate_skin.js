42a17daac3111dc471b1acf050ac06dff1180fe1
Fr33Ze06 <118484751+Fr33Ze06@users.noreply.github.com>
2023-11-16 17:36:16 +0100

    Presque alphapack

--- static/javascript/generate_skin.js
+++ static/javascript/generate_skin.js
@@ -1,12 +1,5 @@
 function openCase() {
     
-    // Exemple d'utilisation
-    const dropRates = {
-        "skin1": 0.01,
-        "skin2": 0.15,
-        "skin3": 0.10,
-        "skin4": 0.74
-    };
     
     const totalSkinsToGenerate = 100;
     const generatedSkins = generateSkinList(dropRates, totalSkinsToGenerate);
@@ -20,18 +13,25 @@ function openCase() {
     resultContainer.innerHTML = 'Congratulations! You got something awesome! U got ' + generatedSkins[randomIndex]; ;
 }
         
-function generateSkinList(dropsData, totalSkins) {
+function generateSkinList(dropsData) {
     // Initialisation de la liste des skins
     let skinList = [];
     
     // Calcul de la somme totale des pourcentages
-    const totalPercentage = dropsData.reduce((acc, drop) => acc + drop.droprate/100, 0);
-    // if (totalPercentage != 1){
-    //     throw new Error("Pourcentage must be 100 percent");
-    // }
+    const totalPercentage = dropsData.reduce((acc, drop) => acc + drop.droprate, 0);
+    if (totalPercentage != 1){
+        throw new Error("Pourcentage must be 100 percent  (" + totalPercentage + ")");
+    }
     
     // Calcul du nombre d'éléments à générer pour chaque skin ("skin4:74")
-    const skinCounts = Object.fromEntries(dropsData.map(item => [item.name, Math.round((item.droprate / totalPercentage) * totalSkins)]));
+    const minDroprate = Math.min(...dropsData.map(item => item.droprate));
+    let totalSkins = 1;
+    while (dropsData.some(item => Math.round((item.droprate / minDroprate) * totalSkins) < 1)) {
+        totalSkins *= 2;
+        console.log(totalSkins)
+    }
+    const skinCounts = Object.fromEntries(dropsData.map(item => [ item.name + item.id.toString(), Math.round((item.droprate / minDroprate) * totalSkins)]));
+    console.log(skinCounts);
     
     // Correction du nombre d'éléments pour s'assurer que le total est exactement égal à totalSkins
     const diff = totalSkins - Object.values(skinCounts).reduce((acc, count) => acc + count, 0);
@@ -70,19 +70,19 @@ function count_element(liste, element){
 
 window.onload = function () {
     const dropsData = JSON.parse(document.getElementById("dropsData").getAttribute("dropsData"));
-    console.log(dropsData);
-    const totalPercentage = dropsData.reduce((acc, drop) => acc + drop.droprate, 0);
-    console.log(totalPercentage);
-    const minDroprate = Math.min(...dropsData.map(item => item.droprate));
-    console.log(minDroprate);
-    let totalSkins = 1;
-    console.log(totalSkins);
-    while (dropsData.some(item => Math.round((item.droprate / minDroprate) * totalSkins) < 1)) {
-        totalSkins *= 2;
-    }
-    const skinCounts = Object.fromEntries(dropsData.map(item => [item.name, Math.round((item.droprate / minDroprate) * totalSkins)]));
-    console.log(skinCounts);
 
-    console.log(generateSkinList(dropsData))
+    ListeSkins = generateSkinList(dropsData);
+    console.log(ListeSkins);
+
+    const divSkins = document.getElementById("wheel")
+
+    ListeSkins.forEach(function(skin) {
+        var divContainer = document.createElement("div");
+        divContainer.className = "wheel-slice";
+        var imgSkin = document.createElement("img");
+        imgSkin.src = "/SkinR6/L85A2/Black_Ice_L85A2_Skin.png";
+        divContainer.appendChild(imgSkin);
+        divSkins.appendChild(divContainer);
+    });
 
 };
